3650d6ac287a69fe31e1101747cf96fc9e28bd6e
onezero1010101 <onezero1010101@gmail.com>
2019-11-26 10:59:46 -0500

    Update backup-sc.sh

--- backup-sc.sh
+++ backup-sc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #tar -cvf scbackup.tar web.config App_ClientConfig App_Data App_Extensions App_WebResources
-name=~/scbackup$(date '+%Y-%m-%d')
+name=~/scbackup_$(date '+%Y-%m-%d')
 cd /var/lib/docker/volumes/screenconnect_vol/_data/
 docker stop screenconnect
 tar -zcvf "$name.tar.gz" web.config App_ClientConfig App_Data App_Extensions App_WebResources
 docker start screenconnect
+ls -1 /root/scbackup_* | sort -r | tail -n +6 | xargs rm > /dev/null 2>&1
