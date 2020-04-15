bba7fc3aab3b3ab827bcbf893194f341730f0e2b
onezero1010101 <onezero1010101@gmail.com>
2020-04-15 09:58:57 -0400

    Add App_Themes to backup

--- backup-sc.sh
+++ backup-sc.sh
@@ -3,6 +3,6 @@
 name=~/scbackup_$(date '+%Y-%m-%d')
 cd /var/lib/docker/volumes/screenconnect_vol/_data/
 docker stop screenconnect
-tar -zcvf "$name.tar.gz" web.config App_ClientConfig App_Data App_Extensions App_WebResources
+tar -zcvf "$name.tar.gz" web.config App_ClientConfig App_Data App_Extensions App_WebResources App_Themes
 docker start screenconnect
 ls -1 /root/scbackup_* | sort -r | tail -n +6 | xargs rm > /dev/null 2>&1
