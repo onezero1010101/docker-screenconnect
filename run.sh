786f46b7866f672d8edd9fe8dce9cecbd8c9b95e
onezero1010101 <onezero1010101@gmail.com>
2020-04-15 10:00:56 -0400

    update for 19.6+

--- run.sh
+++ run.sh
@@ -6,5 +6,11 @@ export MONO_PATH="$screenconnectPath/App_Runtime/lib"
 export MONO_CFG_DIR="$screenconnectPath/App_Runtime/etc"
 export XDG_CONFIG_HOME="$screenconnectPath/App_Runtime/etc"
 export MONO_XMLSERIALIZER_THS=no
+export MONO_ASPNET_WEBCONFIG_CACHESIZE=50000
+export MONO_MANAGED_WATCHER=true
+
+#added to allow needed
+#sysctl fs.inotify.max_user_instances=100000
+export DOTNET_USE_POLLING_FILE_WATCHER=true
 
 mono "$screenconnectPath/Bin/ScreenConnect.Service.exe" startservices 7 12345 10
